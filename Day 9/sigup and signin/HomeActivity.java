3af5982f9248f7e13ad73b545d7f47f08821f892
Ashmine12 <84777491+Ashmine12@users.noreply.github.com>
2021-06-12 10:47:51 +0530

    Update HomeActivity.java

--- Day 9/sigup and signin/HomeActivity.java
+++ Day 9/sigup and signin/HomeActivity.java
@@ -9,15 +9,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-    TextView tv1, tv2, tv3, tv4;
-    Button button;
-
-    public static final String main_key = "mypref";
-    public static final String name_key = "name";
-    public static final String email_key = "email";
-    public static final String password_key = "password";
-    public static final String phone_key = "phone";
-
+    TextView tv1,tv2,tv3,tv4;
+    Button b1;
+    public static final String main_key = "myPref";
+    public static final String name_key = "namekey";
+    public static final String email_key = "emailkey";
+    public static final String password_key = "passwordkey";
+    public static final String mobile_key = "mobilekey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +25,23 @@ public class HomeActivity extends AppCompatActivity {
         tv2 = findViewById(R.id.tv2);
         tv3 = findViewById(R.id.tv3);
         tv4 = findViewById(R.id.tv4);
-        button = findViewById(R.id.button);
+        b1 = findViewById(R.id.b1);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(main_key, MODE_PRIVATE);
-        String name = sharedPreferences.getString(name_key, "");
-        String email = sharedPreferences.getString(email_key, "");
-        String password = sharedPreferences.getString(password_key, "");
-        String phone = sharedPreferences.getString(phone_key, "");
-        tv1.setText("Name:- " + name);
-        tv2.setText("Email:- " + email);
-        tv3.setText("Password:- " + password);
-        tv4.setText("Phone:- " + phone);
+        SharedPreferences sharedPreferences = getSharedPreferences(main_key,MODE_PRIVATE);
+        String name = sharedPreferences.getString(name_key,"");
+        String email = sharedPreferences.getString(email_key,"");
+        String password = sharedPreferences.getString(password_key,"");
+        String mobile = sharedPreferences.getString(mobile_key,"");
+        tv1.setText("Name:- "+name);
+        tv2.setText("Email:- "+email);
+        tv3.setText("Password:- "+password);
+        tv4.setText("Phone:- "+mobile);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences(main_key, MODE_PRIVATE);
+
+                SharedPreferences sharedPreferences = getSharedPreferences(main_key,MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.clear();
                 editor.commit();
