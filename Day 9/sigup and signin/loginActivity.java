399d982be9a61cd44c155e888868ccb8f0e9478b
Ashmine12 <84777491+Ashmine12@users.noreply.github.com>
2021-06-12 10:51:02 +0530

    Update loginActivity.java

--- Day 9/sigup and signin/loginActivity.java
+++ Day 9/sigup and signin/loginActivity.java
@@ -11,53 +11,51 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
-EditText etemail,etpassword;
-Button btnsignin,btnsignup;
+    EditText et1,et2;
+    Button b1,b2;
 
-    public static final String main_key="mypref";
-    public static final String email_key="email";
-    public static final String password_key="password";
-
-
+    public static final String main_key = "myPref";
+    public static final String email_key = "emailkey";
+    public static final String password_key = "passwordkey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        etemail=findViewById(R.id.etemail);
-        etemail=findViewById(R.id.etpassword);
-        btnsignin=findViewById(R.id.btnsignin);
-        btnsignup=findViewById(R.id.btnsignup);
 
-        btnsignin.setOnClickListener(new View.OnClickListener() {
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String useremail,userpassword;
-                useremail=etemail.getText().toString();
-                userpassword=etpassword.getText().toString();
+                useremail = et1.getText().toString();
+                userpassword = et2.getText().toString();
 
-                SharedPreferences sharedPreferences=getSharedPreferences(main_key,MODE_PRIVATE);
-                String email= sharedPreferences.getString(email_key,"");
-                String password=sharedPreferences.getString(password_key,"");
+                SharedPreferences sharedPreferences = getSharedPreferences(main_key,MODE_PRIVATE);
+                String email = sharedPreferences.getString(email_key,"");
+                String password = sharedPreferences.getString(password_key,"");
 
-                if (useremail.equals(email) && userpassword.equals(password)){
-                    Intent intent=new Intent(loginActivity.this,HomeActivity.class);
+                if (useremail.equals(email) && userpassword.equals(password))
+                {
+                    Intent intent = new Intent(loginActivity.this,HomeActivity.class);
                     startActivity(intent);
                 }
-                else {
-                    Toast.makeText(loginActivity.this, "invalid login", Toast.LENGTH_SHORT).show();
+                else{
+                    Toast.makeText(loginActivity.this,"Invaild Email and Password",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-
-        btnsignup.setOnClickListener(new View.OnClickListener() {
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(loginActivity.this,SignupActivity.class);
+                Intent intent = new Intent(loginActivity.this,SignupActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 }
